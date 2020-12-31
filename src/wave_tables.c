e74d6558e092c87eadec7e7de8df947f85f5b919
Johan Fransson <per.johan.fransson@gmail.com>
2020-12-31 20:45:49 +0100

    ok then

--- src/wave_tables.c
+++ src/wave_tables.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include "wave_tables.h"
 
-#define _WAVE_TABLE_HARMONICS 64
-#define _WAVE_TABLE_SIZE 32768
+#define _WAVE_TABLE_HARMONICS 32
+#define _WAVE_TABLE_HARMONIC_STEP (16000/_WAVE_TABLE_HARMONICS)
+#define _WAVE_TABLE_SIZE 48000
 double _wave_square_table[_WAVE_TABLE_HARMONICS][_WAVE_TABLE_SIZE];
 double _wave_saw_table[_WAVE_TABLE_HARMONICS][_WAVE_TABLE_SIZE];
 
@@ -13,7 +14,9 @@ double _indexed_sin(int index) {
 
 void wave_tables_init() {
     for (int harmonic = 0; harmonic < _WAVE_TABLE_HARMONICS; harmonic++) {
-        double base_f = 1.0 + harmonic * 200.0;  //(0-25600 Hz)
+        // TODO non linear harmonic distribution..
+        printf("Generating wave tables %d/%d\n", harmonic+1, _WAVE_TABLE_HARMONICS);
+        double base_f = 1.0 + harmonic * _WAVE_TABLE_HARMONIC_STEP;  //(0-25600 Hz)
         for (int i = 0; i < _WAVE_TABLE_SIZE; i++) {
             //_wave_square_table[harmonic][i] = _indexed_sin(i);
             int h1 = 1;
@@ -25,12 +28,12 @@ void wave_tables_init() {
             h1 = 1;
             int sign = -1;
             _wave_saw_table[harmonic][i] = 0;
-            while (h1 * base_f < 18000) {
+            while (h1 * base_f < 20000) {
                 _wave_saw_table[harmonic][i] += sign * _indexed_sin(i * h1)/(double)h1;
                 sign *= -1;
                 h1++;
             }
-            _wave_saw_table[harmonic][i] *= 0.52;
+            _wave_saw_table[harmonic][i] *= 0.53;
 
             if (fabs(_wave_square_table[harmonic][i]) > 1.0) {
                 fprintf(stderr, "Square overflow detected, harmonic %d, index %d, value %f\n", harmonic, i, _wave_square_table[harmonic][i]);
@@ -43,7 +46,7 @@ void wave_tables_init() {
 }
 
 double wave_table_square(double frequency, double t) {
-    int harmonic = frequency / 200.0;
+    int harmonic = frequency / _WAVE_TABLE_HARMONIC_STEP;
     if (harmonic < 0) {
         harmonic = 0;
     }
@@ -56,7 +59,7 @@ double wave_table_square(double frequency, double t) {
 }
 
 double wave_table_saw(double frequency, double t) {
-    int harmonic = frequency / 200.0;
+    int harmonic = frequency / _WAVE_TABLE_HARMONIC_STEP;
     if (harmonic < 0) {
         harmonic = 0;
     }
